d52421704c8ac92480b0bcd30d970f75ce357299
Adarsh Jadhav <addyjadhav25@gmail.com>
2023-09-27 23:35:48 +0530

    Add 'frontend/' directory

---
+++ frontend/build/precache-manifest.bb9cf593ed129dd07c4ee3b620a34a87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe25cfc99878236eb0e8150ad459b430",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fb2f0b52b1e594650ce",
+    "url": "/static/css/main.dbf46efa.chunk.css"
+  },
+  {
+    "revision": "1f911c779bc16728b3a3",
+    "url": "/static/js/2.2004f6e8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.2004f6e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fb2f0b52b1e594650ce",
+    "url": "/static/js/main.1ea35a6e.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  }
+]);
